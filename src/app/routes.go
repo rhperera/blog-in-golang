4336e29c7f047f93e7c3aec824a4f876c36de4fb
Raveen Perera <raveenp@RaveenPs-Mac-mini.local>
2016-02-05 15:21:56 +0530

    new route file added

--- src/app/routes.go
+++ src/app/routes.go
@@ -8,6 +8,23 @@ import (
 type Route struct{
   Name string
   Method string
-  Pattern string
-  HandlerFunc http.HandlerFunc 
+  Path string
+  Handler http.HandlerFunc
+}
+
+type Routes []Route
+
+func CreateNewRouter(routes Routes) *mux.Router {
+
+  router := mux.NewRouter().strictSlash(true)
+  for _, route := range routes {
+      router
+      .Methods(route.Method)
+      .Path(route.Path)
+      .Name(route.Name)
+      .Handler(router.Handler)
+  }
+
+  return router
+
 }
